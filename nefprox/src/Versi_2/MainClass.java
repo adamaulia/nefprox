4b07aa9f5275baf39d9169bcc46d76a6d164ed15
Muhammd Mushlikh <emush1994@gmail.com>
2015-12-06 01:40:03 +0700

    Sharing Weigt on Input Done

--- nefprox/src/Versi_2/MainClass.java
+++ nefprox/src/Versi_2/MainClass.java
@@ -30,10 +30,27 @@ public class MainClass {
         boolean temp=false;
         //False : belum ada
         //True  : sudah ada
-        
+        String[][] tempRuleInput=inputRule.getRule();
         for (int i=0; i<rules.size(); i++){
-            if(inputRule.getRule()==rules.get(i).getRule()){
+            String[][] tempRule=rules.get(i).getRule();
+            if( (tempRuleInput[0][0]==tempRule[0][0]) &&
+                    (tempRuleInput[1][0]==tempRule[1][0]) &&
+                    (tempRuleInput[2][0]==tempRule[2][0]) &&
+                    (tempRuleInput[3][0]==tempRule[3][0]) ){
+                
                 temp=true;
+                if (Double.parseDouble(tempRuleInput[0][1])>Double.parseDouble(tempRule[0][1])){
+                    tempRule[0][1]=tempRuleInput[0][1];
+                }
+                if (Double.parseDouble(tempRuleInput[1][1])>Double.parseDouble(tempRule[1][1])){
+                    tempRule[1][1]=tempRuleInput[1][1];
+                }
+                if (Double.parseDouble(tempRuleInput[2][1])>Double.parseDouble(tempRule[2][1])){
+                    tempRule[2][1]=tempRuleInput[2][1];
+                }
+                if (Double.parseDouble(tempRuleInput[3][1])>Double.parseDouble(tempRule[3][1])){
+                    tempRule[3][1]=tempRuleInput[3][1];
+                }
             }
         }
         return temp;
@@ -64,6 +81,45 @@ public class MainClass {
         }
     }
     
+    public static void shareWeight(){
+        for(int i=0; i<rules.size(); i++){
+            String[][] tempRule1=rules.get(i).getRule();
+            for(int j=0; j<rules.size(); j++){
+                String max;
+                String[][] tempRule2=rules.get(j).getRule();
+                if(tempRule1[0][0].equals(tempRule2[0][0])){
+                    if(Double.parseDouble(tempRule1[0][1])>Double.parseDouble(tempRule2[0][1])){
+                        max=tempRule1[0][1];
+                    }else{
+                        max=tempRule2[0][1];
+                    }
+                    rules.get(i).getRule()[0][1]=max;
+                    rules.get(j).getRule()[0][1]=max;
+                }
+                
+                if(tempRule1[1][0].equals(tempRule2[1][0])){
+                    if(Double.parseDouble(tempRule1[1][1])>Double.parseDouble(tempRule2[1][1])){
+                        max=tempRule1[1][1];
+                    }else{
+                        max=tempRule2[1][1];
+                    }
+                    rules.get(i).getRule()[1][1]=max;
+                    rules.get(j).getRule()[1][1]=max;
+                }
+                
+                if(tempRule1[2][0].equals(tempRule2[2][0])){
+                    if(Double.parseDouble(tempRule1[2][1])>Double.parseDouble(tempRule2[2][1])){
+                        max=tempRule1[2][1];
+                    }else{
+                        max=tempRule2[2][1];
+                    }
+                    rules.get(i).getRule()[2][1]=max;
+                    rules.get(j).getRule()[2][1]=max;
+                }
+            }
+        }
+    }
+    
     public static void main(String[] args) {
         Data data = new Data();
         data.loadData("src\\data\\BUNDESBANK-BBK01_WT5511.xls");
@@ -79,6 +135,7 @@ public class MainClass {
         fuzzy = new Fuzzifikasi(fk,data.getMax(),data.getMin());
         makeFuzzyInput(data.getAllData());
         makeRule();
+        shareWeight();
         printRule();
     }
 
