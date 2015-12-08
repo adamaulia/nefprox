8e3909c105cb517882879778dbd96b5d97eef74e
adam aulia rahmadi <adamaulia242@gmail.com>
2015-12-08 07:45:25 +0700

    tambah defuzzyfikasi

--- nefprox/src/Versi_2/Rule.java
+++ nefprox/src/Versi_2/Rule.java
@@ -12,87 +12,91 @@ import java.util.Arrays;
  * @author mickeyMice
  */
 public class Rule {
-    String[] x1= new String[2];
-    String[] x2= new String[2];
-    String[] x3= new String[2];
-    String[] target= new String[2];
-    
-    public Rule(){}
-    
-    public Rule(double[] input1,double[] input2,double[] input3,double[] input4){
+
+    private String[] x1 = new String[2];
+    private String[] x2 = new String[2];
+    private String[] x3 = new String[2];
+    private String[] target = new String[2];
+
+    public Rule() {
+    }
+
+    public Rule(double[] input1, double[] input2, double[] input3, double[] input4) {
         x1 = Choose(input1);
         x2 = Choose(input2);
         x3 = Choose(input3);
         //target = Choose(input4);
-       target=konsekuen(x1,x2,x3);
+        target = konsekuen(x1, x2, x3);
     }
-    
-    public void setRule(double[] input1,double[] input2,double[] input3,double[] input4){
+
+    public void setRule(double[] input1, double[] input2, double[] input3, double[] input4) {
         x1 = Choose(input1);
         x2 = Choose(input2);
         x3 = Choose(input3);
         //target = Choose(input4);
-        target=konsekuen(x1,x2,x3);
+        target = konsekuen(x1, x2, x3);
     }
-    
-    public String[] Choose(double[] input){
+
+    public String[] Choose(double[] input) {
         double max = input[0];
-        int idx=0;
+        int idx = 0;
         String temp[] = new String[2];
-        for(int i=0; i<input.length; i++){
-            if (input[i]>max){
-                max=input[i];
-                idx=i;
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] > max) {
+                max = input[i];
+                idx = i;
             }
         }
-        if(idx==0){
-            temp[0]="Rendah";
-        }else if(idx==1){
-            temp[0]="Sedang";
-        }else{
-            temp[0]="Tinggi";
+        if (idx == 0) {
+            temp[0] = "Rendah";
+        } else if (idx == 1) {
+            temp[0] = "Sedang";
+        } else {
+            temp[0] = "Tinggi";
         }
-        
-        temp[1]=String.valueOf(max);
+
+        temp[1] = String.valueOf(max);
         return temp;
-        
+
     }
-    
-    public String[] konsekuen(String[] X1, String[] X2,String[] X3){
-        double d1,d2,d3;
-        double [] tempdouble = new double[3];
-        String [] output = new String [2];
-        String [] tempoutput = new String[3];
-        int rendah=0,sedang=0,tinggi=0;
-        
-        d1=Double.valueOf(X1[1]);
-        d2=Double.valueOf(X2[1]);
-        d3=Double.valueOf(X3[1]);
-        
-        tempdouble[0]=d1;
-        tempdouble[1]=d2;
-        tempdouble[2]=d3;
-        
+
+    public String[] konsekuen(String[] X1, String[] X2, String[] X3) {
+        double d1, d2, d3;
+        double[] tempdouble = new double[3];
+        String[] output = new String[2];
+        String[] tempoutput = new String[3];
+        int rendah = 0, sedang = 0, tinggi = 0;
+
+        d1 = Double.valueOf(X1[1]);
+        d2 = Double.valueOf(X2[1]);
+        d3 = Double.valueOf(X3[1]);
+
+        tempdouble[0] = d1;
+        tempdouble[1] = d2;
+        tempdouble[2] = d3;
+
         Arrays.sort(tempdouble);
-        output[0]=tempdouble[0]+"";
-        
-        tempoutput[0]=X1[0];
-        tempoutput[1]=X2[0];
-        tempoutput[2]=X3[0];
-        
+//        System.out.println(tempdouble[0]+" "+tempdouble[1]+" "+tempdouble[2]);
+        output[0] = tempdouble[0] + "";
+
+        tempoutput[0] = X1[0];
+        tempoutput[1] = X2[0];
+        tempoutput[2] = X3[0];
+
         for (int i = 0; i < tempoutput.length; i++) {
-            if(tempoutput[i]=="Rendah"){
-                rendah=rendah+1;
-            }else if(tempoutput[i]=="Sedang"){
-                sedang=sedang+1;
-            }else if(tempoutput[i]=="Tinggi"){
-                tinggi=tinggi+1;
+            if (tempoutput[i].equals("Rendah")) {
+                rendah = rendah + 1;
+            } else if (tempoutput[i].equals("Sedang")) {
+                sedang = sedang + 1;
+            } else if (tempoutput[i].equals("Tinggi")) {
+                tinggi = tinggi + 1;
             }
         }
-        
+
 //        System.out.println("X1[0]"+X1[0]);
 //        System.out.println("X1[0]"+X2[0]);
 //        System.out.println("X1[0]"+X3[0]);
+
         
         //menentukan hasil variabel linguistik sedang,rendah,tinggi
         
@@ -114,9 +118,28 @@ public class Rule {
             output[1]="sedang";
         }else if((tinggi==2)&&(rendah==1)){
             output[1]="tinggi";
+
+        if (tinggi == 3) {
+            output[1] = "Tinggi";
+        } else if (sedang == 3) {
+            output[1] = "Sedang";
+        } else if (rendah == 3) {
+            output[1] = "Rendah";
+        } else if ((rendah == 2) && (sedang == 1)) {
+            output[1] = "Rendah";
+        } else if ((sedang == 2) && (rendah == 1)) {
+            output[1] = "Sedang";
+        } else if ((sedang == 2) && (tinggi == 1)) {
+            output[1] = "Sedang";
+        } else if ((tinggi == 2) && (sedang == 1)) {
+            output[1] = "Tinggi";
+        } else if ((rendah == 2) && (tinggi == 1)) {
+            output[1] = "Sedang";
+        } else if ((tinggi == 2) && (rendah == 1)) {
+            output[1] = "Tinggi";
+
         }
-        
-        
+
 //        if(tempdouble[0]<0.3){
 //            output[1]="Rendah";
 //        }else if((tempdouble[0]>0.3)&&(tempdouble[0]<=0.6)){
@@ -125,18 +148,18 @@ public class Rule {
 //            output[1]="Sedang";
 //        }
         
-        
-        
-        return output;
+        }
+		return output;
     }
-    
-    public String[][] getRule(){
+
+    public String[][] getRule() {
         String temp[][] = new String[4][2];
-        temp[0]=x1;
-        temp[1]=x2;
-        temp[2]=x3;
-        temp[3]=target;
-        
+        temp[0] = x1;
+        temp[1] = x2;
+        temp[2] = x3;
+        temp[3] = konsekuen(x1,x2,x3);
+//        temp[3] = target;
+
         return temp;
     }
 }
