a5578f29802425610de58dbc3d174786ced49504
adam aulia rahmadi <adamaulia242@gmail.com>
2015-12-15 00:50:00 +0700

    defuzzyfikasi di v3

--- src/Versi_3/Fuzzy.java
+++ src/Versi_3/Fuzzy.java
@@ -65,10 +65,29 @@ public class Fuzzy {
         return tempOutput;
     }
     
-    public double defuzzyfikasi(double input){
-        double temp=0;
+    public double dodefuzyfikasi(double[] input){
+        double tempRendah=0;
+        double tempSedang=0;
+        double tempTinggi=0;
+        double output;
+        //rendah 10,20,30
+        //sedang 40,50,60,70
+        //tinggi 80,90,100
         
-        return temp;
+        for (int i = 0; i <=100; i+=10) {
+            if(i<40){
+                tempRendah = tempRendah+tempRendah*input[0]; //rendah
+            }if (i >=40 && i<80){
+                tempSedang = tempSedang+tempSedang*input[1]; //sedang
+            }
+            if(i >=80){
+                tempTinggi = tempTinggi+tempTinggi*input[2]; //tinggi
+            }
+        }
+            
+        
+        output=tempRendah+tempSedang+tempTinggi/((input[0]*3)+(input[1]*4)+(input[2]*3));
+        return output; 
     }
     
 }
